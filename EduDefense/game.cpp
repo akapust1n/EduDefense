3ad2553a1446e58c2fd0cc60909b210406aafa8b
alexey <djkah11@yandex.ru>
2016-04-17 17:34:27 +0300

    remove trash files

--- EduDefense/game.cpp
+++ EduDefense/game.cpp
@@ -1,36 +1,39 @@
+#include "map.h"
 #include <game.h>
 #include <iostream>
-#include "map.h"
 using namespace std;
 Game::Game(size_t width, size_t height)
-    : window(sf::VideoMode(width, height), GAME_TITLE)
-    , window_width(width)
-    , window_height(height)
-    , menu(width, height) {
-
-}
+    : window(sf::VideoMode(width, height), GAME_TITLE), window_width(width),
+      window_height(height), menu(width, height), m_Exit(false) {}
 
 void Game::run() {
-    switch (menu.process(window)) {
-    case PLAY: {
-        window.clear();
-        Waiter waiter;
-        UsualMapBuilder umap(window,"maps/map.png");
-        waiter.SetMapBuilder(&umap);
-        waiter.ConstructPizza();
-        break;
-    }
-    case OPTIONS:
-        cout << "OPTIONS";
-        break;
-    case STATS:
-        cout << "STATS";
-        break;
-    case QUIT:
-        cout << "QUIT";
-        return;
-    default:
-        cout << "This should not happen";
-        break;
+    while (window.isOpen() && !m_Exit) {
+        sf::Event event;
+       /* switch (event.type) {
+        case sf::Event::Closed:
+            window.close();
+        }*/
+        switch (menu.process(window)) {
+        case PLAY: {
+            window.clear();
+            Waiter waiter;
+            UsualMapBuilder umap(window, "maps/map.png");
+            waiter.SetMapBuilder(&umap);
+            waiter.ConstructPizza();
+            break;
+        }
+        case OPTIONS:
+            cout << "OPTIONS";
+            break;
+        case STATS:
+            cout << "STATS";
+            break;
+        case QUIT:
+            cout << "QUIT";
+            return;
+        default:
+            cout << "This should not happen";
+            break;
+        }
     }
 }
--- EduDefense/game.h
+++ EduDefense/game.h
@@ -1,16 +1,18 @@
-#include <SFML/Graphics.hpp>
 #include "menu.h"
+#include <SFML/Graphics.hpp>
 
 #define GAME_TITLE "Edu Defense"
 
-class  Game {
-public:
-     Game(size_t width, size_t height);
-     void run();
-     Menu menu;
+class Game {
+  public:
+    Game(size_t width, size_t height);
+    void run();
+    Menu menu;
+    void Exit() { m_Exit = true; }
 
-private:
-     size_t window_width;
-     size_t window_height;
-     sf::RenderWindow window;
+  private:
+    size_t window_width;
+    size_t window_height;
+    sf::RenderWindow window;
+    bool m_Exit;
 };
