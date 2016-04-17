9d72ffca8a2fb22e793f3fdb9bcb282064f3fb75
rashemihmih <mihmih96@gmail.com>
2016-04-17 19:21:22 +0300

    added some logic to class Missle

--- EduDefense/missle.h
+++ EduDefense/missle.h
@@ -4,15 +4,22 @@
 #include <cstring>
 #include "moveable.h"
 #include "activegameobject.h"
+#include "enemy.h"
 
 class Missle : public Moveable, public ActiveGameObject {
 public:
     virtual ~Missle() = 0;
     double getDamage();
     void setDamage(double damage);
-
+    Enemy *getTarget();
+    void setTarget(Enemy *target);
+    bool isExploded();
 protected:
     double damage;
+    Enemy *target;
+    bool exploded = false;
+    void hitTarget();
+    void action() override;
 };
 
 #endif // MISSLE_H
