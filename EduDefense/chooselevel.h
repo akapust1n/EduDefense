ed7eac27be78665c8b2facc2a1e43087c90fc588
alexey <djkah11@yandex.ru>
2016-04-17 19:59:00 +0300

    remove pollevent

--- EduDefense/chooselevel.h
+++ EduDefense/chooselevel.h
@@ -6,7 +6,7 @@
 class ChooseLevel{
 public:
     void loadLevel(sf::RenderWindow &window );
-    void loadMenuLevels(sf:RenderWindow &window);
+    void loadMenuLevels(sf::sRenderWindow &window);
 };
 
 #endif // CHOOSELEVEL_H
